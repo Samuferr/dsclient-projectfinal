412b0a7782f0a0e1161a2a4c89afbe9c5c3a6f03
Samuferr <samuferr@gmail.com>
2021-01-30 16:45:24 -0300

    Database seeding

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,1 +1,3 @@
-INSERT INTO tb_clients
+INSERT INTO tb_client (name) VALUES ('Joaquim Guilherme');
+INSERT INTO tb_client (name) VALUES ('Maria Júlia');
+INSERT INTO tb_client (name) VALUES ('Arthur Miguel');
