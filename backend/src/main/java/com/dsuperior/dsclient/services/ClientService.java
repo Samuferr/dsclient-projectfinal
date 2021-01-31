9fcad016418e79710c6701773c0df669d0edda8f
Samuferr <samuferr@gmail.com>
2021-01-31 15:46:31 -0300

    POST nem Client

--- backend/src/main/java/com/dsuperior/dsclient/services/ClientService.java
+++ backend/src/main/java/com/dsuperior/dsclient/services/ClientService.java
@@ -1,11 +1,14 @@
 package com.dsuperior.dsclient.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.dsuperior.dsclient.dto.ClientDTO;
 import com.dsuperior.dsclient.entities.Client;
@@ -24,4 +27,18 @@ public class ClientService {
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
 
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.get();
+		return new ClientDTO(entity);	
+	}
+
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
 }
