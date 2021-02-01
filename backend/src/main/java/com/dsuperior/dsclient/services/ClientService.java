e599499f7e4c39b9762e34fd6f397456752d5b31
Samuel Ferreira <samuferr@gmail.com>
2021-02-01 20:20:53 -0200

    Pagination

--- backend/src/main/java/com/dsuperior/dsclient/services/ClientService.java
+++ backend/src/main/java/com/dsuperior/dsclient/services/ClientService.java
@@ -1,10 +1,10 @@
 package com.dsuperior.dsclient.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,10 +19,10 @@ public class ClientService {
 	private ClientRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<ClientDTO> findAll() {
-		List<Client> list = repository.findAll();
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
 		
-		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+		return list.map(x -> new ClientDTO(x));
 	}
 
 	@Transactional(readOnly = true)
